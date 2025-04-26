983eff173201b0db6f01f5ae0591194f1916e5a6
GamerCleanVic <jonassd2000@gmail.com>
2025-04-25 22:25:49 -0500

    indexar

---
+++ a4_indexadas.py
@@ -0,0 +1,12 @@
+palavra = 'Fithom'
+print('palavra = "Fithom"')
+print(palavra)
+palavra = 'P' + palavra[1:]
+print('palavra = "P" + palavra[1:]')
+print(palavra)
+palavra = palavra[:1] + 'y' + palavra[2:]
+print('palavra = palavra[:1] + "y" + palavra[2:]')
+print(palavra)
+palavra = palavra[:5] + 'n'
+print('palavra = palavra[:5] + "n"')
+print(palavra)
